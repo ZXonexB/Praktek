e92771d4c9ef05083030d108c6a3fa92a37c9a0f
ZXonexB <gzyzg2021@gmail.com>
2023-12-18 11:18:50 +0700

    Ujian praktek

---
+++ ujian.php
@@ -0,0 +1,18 @@
+
+<?php
+echo "<p>saya mengerjakan ujian ini dengan jujur!</p>";
+
+echo "<img src='gambar.jpg' alt='deskripsi'>";
+
+echo "<h2>programming</h2>";
+
+/*
+"sedang mengerjakan soal!"
+"dilarang berisik, ada ujian"
+"tetap fokus dan konsentrasi saat belajar, hindari gangguan dan periksa jadwal secara rutin"
+*/
+
+echo "<textarea placeholder="isi bio"></textarea>";
+
+echo "<style>p {font-size: 50%; color: green;}</style>";
+?>
